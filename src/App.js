098846abc2b5728d59bb3d69c697a743988dda01
Kota Watanabe <kota3316@gmail.com>
2019-06-12 11:11:36 -0700

    implement state for filterdrobots

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots'
 
-const state = {
-    robots: robots,
-    searchfield: ''
-}
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange =(event)=>{
+        this.setState({ searchfield: event.target.value })
+    }
     render(){
+        const filterdRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
         return(
             <div className="tc">
             <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots = {robots}/>
+            <SearchBox searchChange ={this.onSearchChange}/>
+            <CardList robots = {filterdRobots}/>
             </div>
         );
     }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () =>{
+const SearchBox = ({ searchfiled, searchChange }) =>{
     return(
         <div className="pa2">
             <input 
             className="pa3 ba b--green bglightest-blue"
             type="search" 
             placeholder='search robots'
+            onChange={searchChange}
              />
          </div>
     );
