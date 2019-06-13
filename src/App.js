a2ed1153d52981c79193d8ddf98b0b1d6a1213be
Kota Watanabe <kota3316@gmail.com>
2019-06-13 10:10:19 -0700

    fetch users data from api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots'
 import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));        
     }
 
     onSearchChange =(event)=>{
@@ -20,13 +25,18 @@ class App extends Component {
         const filterdRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return(
-            <div className="tc">
-            <h1 className="f2">RoboFriends</h1>
-            <SearchBox searchChange ={this.onSearchChange}/>
-            <CardList robots = {filterdRobots}/>
-            </div>
-        );
+        if(this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else{
+            return(
+                <div className="tc">
+                <h1 className="f2">RoboFriends</h1>
+                <SearchBox searchChange ={this.onSearchChange}/>
+                <CardList robots = {filterdRobots}/>
+                </div>
+            );
+        }
+
     }
 
 }
